1ccb934e10fa4b6cf6a2fefb82a492821c559925
Steevy <twitter@kuechenscha.be>
2023-07-23 23:18:21 +0200

    chore(tests): moving UnitTests into own folder
    according to https://docs.micropython.org/en/v1.15/develop/writingtests.html tests are stored in an own folder ("tests")

--- UnitTest_ControlModule.py
+++
@@ -1,9 +0,0 @@
-from controlModule import ControlModule
-
-if __name__ == '__main__':   # Program entrance
-    controlModule = ControlModule()
-    controlModule.setupManagers()
-    controlModule.establishManagerConnections()
-    controlModule.sensorDM.update(controlModule.sensorDM.moistureSensor)
-    while (True):
-        pass
